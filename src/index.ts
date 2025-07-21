e52dfc4d8cc437b75554902aaff73b3b8ecfb0b9
AdityaSingh-100 <adityasingh902491@gmail.com>
2025-07-21 19:49:21 +0530

    Added middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
-import { UserModel } from "./db";
+import { ContentModel, UserModel } from "./db";
+import { JWT_PASSWORD } from "./config";
+import { userMiddleWare } from "./middleware";
 
 const app = express();
 app.use(express.json());
@@ -25,11 +26,44 @@ app.post("/api/v1/signup", async function (req, res) {
     });
   }
 });
-app.post("/api/v1/signin", function (req, res) {
+
+app.post("/api/v1/signin", async function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
+  const existingUser = await UserModel.findOne({
+    username,
+    password,
+  });
+  if (existingUser) {
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+      },
+      JWT_PASSWORD
+    );
+    res.json({
+      token,
+    });
+  } else {
+    res.status(403).json({
+      message: "Incorrect Credentials",
+    });
+  }
 });
-app.post("/api/v1/content", function (req, res) {});
+
+app.post("/api/v1/content", userMiddleWare, async function (req, res) {
+  const link = req.body.link;
+  const type = req.body.type;
+  await ContentModel.create({
+    link,
+    type,
+    userId: req.userId,
+    tags: [],
+  });
+  res.json({
+    message: "Content Added",
+  });
+});
 app.get("/api/v1/content", function (req, res) {});
 app.delete("/api/v1/content", function (req, res) {});
 app.post("/api/v1/brain/share", function (req, res) {});
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,39 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { decode } from "jsonwebtoken";
+import { JWT_PASSWORD } from "./config";
 
+export const userMiddleWare = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const header = req.headers["authorization"];
+
+  if (!header) {
+    return res.status(401).json({ message: "Authorization header missing" });
+  }
+
+  const decoded = jwt.verify(header as string, JWT_PASSWORD) as {
+    id: string;
+  };
+
+  if (decoded) {
+    req.userId = decoded.id;
+    next();
+  } else {
+    res.status(403).json({ message: "You are not logged in" });
+  }
+};
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string;
+    }
+  }
+}
+// By default, Express's Request object doesn't have a userId property. So if you try to do:
+//This code extends the existing Express Request interface to add a custom property userId of type string
+// req.userId = "some-user-id";  // ❌ TypeScript error: Property 'userId' does not exist
+
+// Overide the types of the express request objects
