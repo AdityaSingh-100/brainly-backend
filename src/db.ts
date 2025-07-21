325f7e01e46340ef3936db19674dbd4fd5dbbf71
AdityaSingh-100 <adityasingh902491@gmail.com>
2025-07-21 18:27:43 +0530

    changed project name

---
+++ src/db.ts
@@ -0,0 +1,12 @@
+import mongoose, { model, Schema } from "mongoose";
+
+mongoose.connect(
+  "mongodb+srv://admin:tSoA4IFvu9Q5ph3m@cluster0.x2erf5e.mongodb.net/brainly"
+);
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true },
+  password: String,
+});
+
+export const UserModel = model("User");
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import { UserModel } from "./db";
 
 const app = express();
+app.use(express.json());
+app.post("/api/v1/signup", async function (req, res) {
+  //TODO - zod validation , hash the password
+  const username = req.body.name;
+  const password = req.body.password;
 
-app.post("/api/v1/signup", function (req, res) {});
-app.post("/api/v1/signin", function (req, res) {});
+  await UserModel.create({
+    username: username,
+    password: password,
+  });
+
+  res.json({
+    message: "User signed up",
+  });
+});
+app.post("/api/v1/signin", function (req, res) {
+  const username = req.body.name;
+  const password = req.body.password;
+});
 app.post("/api/v1/content", function (req, res) {});
 app.get("/api/v1/content", function (req, res) {});
 app.delete("/api/v1/content", function (req, res) {});
